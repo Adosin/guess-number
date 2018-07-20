c33be3bf53f47fd6fececba4ca9b53bd47a0666f
Adosin <Adosin.Lo@gmail.com>
2018-07-20 10:52:18 +0800

    basic versiuon

---
+++ guess.py
@@ -0,0 +1,18 @@
+# 產生一個隨機整數 1 ~ 100
+# 讓使用者"重複(迴圈的意思)"數字去猜
+# 猜對的話, 印出"終於猜對啦!"
+# 猜錯的話, 要告訴使用者答案大小
+
+import random
+
+r = random.randint(1, 100)
+while True:
+	num = input('請猜數字: ')
+	num = int(num)
+	if num == r:
+		print('你猜中了!')
+		break
+	elif num > r:
+		print('數字大於答案')
+	elif num < r: 
+		print('數字小於答案')
